983b7fab9f3628fcc1b5678e9bc217595ec6c443
phiarchitect <phi@phiarchitect.com>
2023-06-07 21:39:40 -0700

    update playlist

--- src/harmonic_resonance/chordulator/templates/script.js
+++ src/harmonic_resonance/chordulator/templates/script.js
@@ -1,7 +1,17 @@
 var urls = {{ files_with_titles|tojson }};
 var index = 0;
+var sectionIndex = 0;
+var sections = document.getElementById('iframe').contentDocument.getElementsByTagName('section');
 function previous() { if (index > 0) { index--; update(); } }
 function next() { if (index < urls.length - 1) { index++; update(); } }
+function scrollSection(dir) {
+  if (dir > 0) {
+    if (sectionIndex < sections.length - 1) { sectionIndex++; }
+  } else {
+    if (sectionIndex > 0) { sectionIndex--; }
+  }
+  sections[sectionIndex].scrollIntoView({behavior: "smooth", block: "center"});
+}
 function update() {
   document.getElementById("iframe").src = urls[index][0];
   var links = document.querySelectorAll("nav a");
@@ -19,5 +29,22 @@ document.querySelectorAll("nav a").forEach(function(link, i) {
     index = i;
     update();
   });
+});
+
+window.addEventListener('keydown', function(event) {
+  switch (event.keyCode) {
+    case 37: // left arrow
+      previous();
+      break;
+    case 39: // right arrow
+      next();
+      break;
+    case 38: // up arrow
+      scrollSection(-1);
+      break;
+    case 40: // down arrow
+      scrollSection(1);
+      break;
+  }
 });
 
