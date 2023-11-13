64dc31c7b89161e5846fa4311eb4fff4255f6efe
phiarchitect <phi@phiarchitect.com>
2023-11-12 17:27:43 -0800

    update content

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -1,22 +1,23 @@
 from ablog.conf import *
 
-org = ""
-org_name = ""
+org = "harmonic-resonance"
+org_name = "HARMONIC resonance"
 
-repo = ""
-repo_name = ""
+repo = "chordulator"
+repo_name = "chordulator"
 
-blog_title = f"{org_name} • {repo_name}"
-project = f"{org_name}<br/>•<br/>{repo_name}"
-version = ""  # The short X.Y version.
-release = ""  # The full version, including alpha/beta/rc tags.
+blog_title = f'{org_name} • {repo_name}'
+html_title = f'{org_name} • {repo_name}'
+project = f'{org_name} • {repo_name}'
+version = ''  # The short X.Y version.
+release = ''  # The full version, including alpha/beta/rc tags.
 
-copyright = f"{year}, {org_name}"
-author = ""
+copyright = f'{year}, {org_name}'
+author = f'{org_name}'
 
 # Base URL for the website, required for generating feeds.
 # e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = f"https://{org}.github.io/{repo}"
+blog_baseurl = f'https://{org}.github.io/{repo}'
 html_base_url = blog_baseurl
 html_baseurl = blog_baseurl
 
@@ -24,10 +25,10 @@ blog_authors = {
     "phi": ("phi ARCHITECT", None),
 }
 
-html_theme_options = {
-    "logo": "logo.png",
-    "logo_name": True,
-    "github_user": org,
-    "github_repo": repo,
-    "github_button": True,
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": org, # Username
+    "github_repo": repo, # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docsrc/", # Path in the checkout to the docs root
 }
